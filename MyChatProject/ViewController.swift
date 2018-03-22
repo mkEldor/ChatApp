41e0b00ae795f2d6a067112a13421ee62dbae00f
Аскар <askar@macbook-pro-askar.local>
2018-03-22 12:45:25 +0600

    Initial Commit

---
+++ MyChatProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyChatProject
+//
+//  Created by Аскар on 22.03.2018.
+//  Copyright © 2018 askar.ulubayev168. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
